3bec50fa7139cc96ae13142ff8189f5b6eb59643
bulerbin420-debug <bulerbin420@gmail.com>
2026-01-11 10:29:57 -0500

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.samektra.compliancelens',
+  appName: 'Samektra Compliance Lens',
+  webDir: 'dist'
+};
+
+export default config;
