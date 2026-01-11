f96b5255ccf9fb9ba9f8a9ff7748931152274013
bulerbin420-debug <bulerbin420@gmail.com>
2026-01-11 10:43:01 -0500

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.samektra.compliancelens',
-  appName: 'Samektra Compliance Lens',
-  webDir: 'dist'
-};
-
-export default config;
